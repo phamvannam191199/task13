16e27400625781a191f385f55492f8095c7d21d3
phamvannam191199 <82355164+phamvannam191199@users.noreply.github.com>
2021-06-04 07:25:11 +0700

    Add files via upload

---
+++ [5a] if-statement.js
@@ -0,0 +1,11 @@
+var score =75;
+var msg;
+if (score >= 50);{
+    msg = 'Congratulation!';
+
+    msg += 'Proceed to the next round.';
+
+}
+var el = document.getElementById('answer');
+    el.textContent = msg;
+
